ee005bff6364d9e096db305983ccf90e6516be0f
Aziszy <abdul.azizz@gmail.com>
2022-05-24 01:37:03 +0700

    ubah kode hanya menampilkan jam dan suhu

--- sistem_alarm_digital.ino
+++ sistem_alarm_digital.ino
@@ -3,6 +3,7 @@
 #include <MD_Parola_lib.h>
 #include <MD_Parola.h>
 #include <DS3231.h>
+#include <Keypad.h>
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
@@ -11,10 +12,25 @@
 #define CS_PIN 10
 #define LDR A1
 
+#define NUM_ROWS 4
+#define NUM_COLS 4
+
 #define BUF_SIZE 75
 #define DELAY 100
 
+char keymap[NUM_ROWS][NUM_COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'},
+};
+
+byte rowPins[NUM_ROWS] = {9, 8, 7, 6};
+byte colPins[NUM_COLS] = {5, 4, 3, 2};
+
 MD_Parola output = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, NUM_ROWS, NUM_COLS);
+
 DS3231 rtc(SDA, SCL);
 Time t;
 
@@ -26,11 +42,11 @@ textEffect_t effectNoEffect = PA_NO_EFFECT;
 textPosition_t textAlign = PA_CENTER;
 uint16_t scrollPause = 3000;
 
-bool systemMainLoop = true;
-bool systemSettingsLoop = false;
-
 bool mainLoop = true;
 bool timeSync = true;
+
+bool systemMainLoop = true;
+bool systemSettingsLoop = false;
 
 int display_scene = 0;
 int display_effect = 0;
@@ -42,14 +58,13 @@ uint8_t intensity;
 uint8_t timeHour;
 uint8_t timeMinute;
 uint8_t timeSecond;
-uint8_t timeDay;
-uint8_t timeMonth;
-uint8_t timeDate;
-word timeYear;
 
-String day, dates, times;
-uint8_t dow, hour, minute, second, date, month, year;
+String times;
+uint8_t hour, minute, second;
 int temp;
+
+String name = "Azis";
+String nrp = "7004";
 
 uint8_t degC[] = {6, 3, 3, 56, 68, 68, 68};
 String daysOfWeek[7] = {
@@ -61,6 +76,10 @@ String daysOfWeek[7] = {
     "Jum",
     "Sab",
 };
+
+char keypressed;
+int pressedCount = 0;
+char c1, c2;
 
 byte setIntensityLed(int light)
 {
@@ -85,102 +104,26 @@ int formatYear(int longYear)
     return longYear % 100;
 }
 
-void adjustClock(String data)
-{
-    byte _day = data.substring(0, 2).toInt();
-    byte _month = data.substring(3, 5).toInt();
-    byte _year = data.substring(6, 10).toInt();
-    byte _hour = data.substring(11, 13).toInt();
-    byte _minute = data.substring(14, 16).toInt();
-    byte _second = data.substring(17, 19).toInt();
-
-    rtc.setTime(_hour, _minute, _second);
-    rtc.setDate(_day, _month, _year);
-    Serial.println("Berhasil");
-}
-
 void setTimeAndTemp()
 {
-    dow = t.dow;
-    day = daysOfWeek[dow];
     hour = t.hour;
     minute = t.min;
     second = t.sec;
-    date = t.date;
-    month = t.mon;
-    year = t.year;
     temp = (int)rtc.getTemp();
 
-    dates = (String)date + '.' + month + '.' + year;
     times = (String)hour + ':' + minute;
 }
 
-void setTextDisplay()
+void setTimer()
 {
-    setTimeAndTemp();
-
-    switch (display_scene)
+    if (second == 10)
     {
-    case 0:
-        strcpy(text, times.c_str());
-        if (display_effect == 0)
-        {
-            output.setTextEffect(PA_OPENING, PA_NO_EFFECT);
-            display_effect = 1;
-        }
-        break;
-    case 1:
-        strcpy(text, rtc.getDOWStr());
-        if (display_effect == 1)
-        {
-            output.setTextEffect(PA_RANDOM, PA_NO_EFFECT);
-            display_effect = 2;
-        }
-        break;
-    case 2:
-        strcpy(text, dates.c_str());
-        if (display_effect == 2)
-        {
-            output.setTextEffect(PA_SCROLL_UP, PA_NO_EFFECT);
-            display_effect = 3;
-        }
-        break;
-    case 3:
-        dtostrf(temp, 0, 0, text);
-        strcat(text, " $");
-        if (display_effect == 3)
-        {
-            output.setTextEffect(PA_SCROLL_DOWN, PA_NO_EFFECT);
-            display_effect = 0;
-        }
-        break;
-    }
-}
-
-void display_scene_setting()
-{
-    if (t.sec >= 0 && t.sec <= 30)
-    {
-        display_scene = 0;
-    }
-    else if (t.sec >= 31 && t.sec <= 40)
-    {
-        display_scene = 1;
-    }
-    else if (t.sec >= 41 && t.sec <= 50)
-    {
-        display_scene = 2;
-    }
-    else if (t.sec >= 51 && t.sec <= 60)
-    {
-        display_scene = 3;
     }
 }
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Masukkan waktu");
     output.begin();
     rtc.begin();
 
@@ -197,50 +140,89 @@ void setup()
 
 void loop()
 {
+    keypressed = myKeypad.getKey();
     intensity = setIntensityLed(analogRead(LDR));
     output.setIntensity(intensity);
-    if (Serial.available() > 0)
-    {
-        adjustClock(Serial.readString());
-    }
-    if (output.displayAnimate())
-    {
-        output.displayText(text, textAlign, scrollSpeed, scrollPause, effectPrint, effectNoEffect);
 
-        t = rtc.getTime();
-
-        if (systemMainLoop == true && systemSettingsLoop == false)
+    while (keypressed == NO_KEY)
+    {
+        if (output.displayAnimate())
         {
-            if (mainLoop == true)
+            output.displayText(text, textAlign, scrollSpeed, scrollPause, effectPrint, effectNoEffect);
+
+            t = rtc.getTime();
+            if (systemMainLoop == true && systemSettingsLoop == false)
             {
-                output.setSpeed(scrollSpeed);
-                output.setPause(scrollPause);
-                output.setTextEffect(0, PA_SCROLL_DOWN, PA_NO_EFFECT);
-                mainLoop = false;
+                if (mainLoop == true)
+                {
+                    output.setSpeed(scrollSpeed);
+                    output.setPause(scrollPause);
+                    output.setTextEffect(0, PA_SCROLL_DOWN, PA_NO_EFFECT);
+                    mainLoop = false;
+                }
+                setTimeAndTemp();
+
+                switch (display_scene)
+                {
+                case 0:
+                    strcpy(text, times.c_str());
+                    if (display_scene == 0)
+                    {
+                        output.setTextEffect(PA_OPENING, PA_NO_EFFECT);
+                        display_effect = 1;
+                    }
+                    break;
+                case 1:
+                    dtostrf(temp, 0, 0, text);
+                    strcat(text, " $");
+                    if (display_scene == 0)
+                    {
+                        output.setTextEffect(PA_RANDOM, PA_NO_EFFECT);
+                        display_effect = 0;
+                    }
+                    break;
+                }
+
+                if (second >= 0 && second <= 30)
+                {
+                    display_scene = 0;
+                }
+                else
+                {
+                    display_scene = 1;
+                }
             }
-            setTextDisplay();
-            display_scene_setting();
+            if (systemMainLoop == false && systemSettingsLoop == true)
+            {
+                if (mainLoop == false)
+                {
+                    output.setSpeed(scrollSpeed);
+                    output.setPause(scrollPause);
+                    output.setTextEffect(0, PA_SCROLL_UP, PA_NO_EFFECT);
+                    mainLoop = true;
+                }
+                char keypressed2 = myKeypad.waitForKey();
+            }
         }
-        if (systemMainLoop == false && systemSettingsLoop == true)
+    }
+
+    if (keypressed == '*')
+    {
+        Serial.println(keypressed);
+        systemMainLoop = false;
+        systemSettingsLoop = true;
+
+        text[0] = timeHour / 10 + 48;
+        text[1] = timeHour % 10 + 48;
+        text[2] = ':';
+        text[3] = '-';
+        text[4] = '-';
+
+        char keypressed2 = myKeypad.waitForKey();
+        if (keypressed2 != NO_KEY && keypressed2 != '*' && keypressed2 != '#' && keypressed2 != 'A' && keypressed2 != 'B' && keypressed2 != 'C' && keypressed2 != 'D')
         {
-            if (mainLoop == false)
-            {
-                output.setSpeed(scrollSpeed);
-                output.setPause(scrollPause);
-                output.setTextEffect(0, PA_SCROLL_UP, PA_NO_EFFECT);
-                mainLoop = true;
-            }
-            if (timeSync == true)
-            {
-                timeHour = hour;
-                timeMinute = minute;
-                timeSecond = second;
-                timeDay = dow;
-                timeMonth = month;
-                timeDate = date;
-                timeYear = year;
-                timeSync = false;
-            }
+            c1 = keypressed2;
+            text[0] = keypressed2;
         }
     }
 }
